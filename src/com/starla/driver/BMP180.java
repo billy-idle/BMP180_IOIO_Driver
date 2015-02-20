379f09b856b6d4a9e025fef217001319e5aa06be
Guille <guillesupremacy@gmail.com>
2015-02-19 20:14:58 -0500

    Javadoc was added.

--- src/com/starla/driver/BMP180.java
+++ src/com/starla/driver/BMP180.java
@@ -3,11 +3,17 @@ package com.starla.driver;
 /**
  * Created by guillesupremacy on 12/2/2015.
  */
+
 import ioio.lib.api.TwiMaster;
 import ioio.lib.api.exception.ConnectionLostException;
-import ioio.lib.util.IOIOConnectionManager.Thread;
 
-public class BMP180 {
+/**
+ * This class is based on SFE_BMP180 library for arduino, designed by Mike Grusin, SparkFun Electronics.
+ *
+ * @version 1, 7/03/14
+ * @author Guillermo Guzm&aacute;n S&aacute;nchez
+ */
+public final class BMP180 {
     private static final int BMP180_ADDRESS = 0x77; // 7-bit address
     private static final int BMP180_REG_CONTROL = 0xF4;
     private static final int BMP180_REG_RESULT = 0xF6;
@@ -43,6 +49,12 @@ public class BMP180 {
     private double p1;
     private double p2;
 
+    /**
+     *
+     * @param twi
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
     public BMP180(TwiMaster twi) throws ConnectionLostException, InterruptedException {
         this.twi = twi;
         if (begin()) {
@@ -52,18 +64,36 @@ public class BMP180 {
         }
     }
 
+    /**
+     *
+     * @return return true if the sensor was correctly initialized.
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
     private boolean begin() throws ConnectionLostException, InterruptedException {
-        if(readInt(AC1_REG_ADDRESS) && readInt(AC2_REG_ADDRESS) && readInt(AC3_REG_ADDRESS) &&
-                readUInt(AC4_REG_ADDRESS) && readUInt(AC5_REG_ADDRESS) &&  readUInt(AC6_REG_ADDRESS) &&
-                readInt(B1_REG_ADDRESS) && readInt(B2_REG_ADDRESS) && readInt(MB_REG_ADDRESS) && readInt(MC_REG_ADDRESS) && readInt(MD_REG_ADDRESS)) {
+        if(readInt(AC1_REG_ADDRESS) &&
+                readInt(AC2_REG_ADDRESS) &&
+                readInt(AC3_REG_ADDRESS) &&
+                readUInt(AC4_REG_ADDRESS) &&
+                readUInt(AC5_REG_ADDRESS) &&
+                readUInt(AC6_REG_ADDRESS) &&
+                readInt(B1_REG_ADDRESS) &&
+                readInt(B2_REG_ADDRESS) &&
+                readInt(MB_REG_ADDRESS) &&
+                readInt(MC_REG_ADDRESS) &&
+                readInt(MD_REG_ADDRESS)) {
             computePolynomials();
             return true;
         }
         return false;
     }
 
-    private void computePolynomials() throws ConnectionLostException,
-            InterruptedException {
+    /**
+     *
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
+    private void computePolynomials() throws ConnectionLostException, InterruptedException {
         double c3, c4, b1;
 
         int AC1 = getIntCoefficient(AC1_REG_ADDRESS);
@@ -77,6 +107,7 @@ public class BMP180 {
         int MB = getIntCoefficient(MB_REG_ADDRESS);
         int MC = getIntCoefficient(MC_REG_ADDRESS);
         int MD = getIntCoefficient(MD_REG_ADDRESS);
+
         /*
 		System.out.println("AC1 = " + AC1);
 		System.out.println("AC2 = " + AC2);
@@ -90,23 +121,25 @@ public class BMP180 {
 		System.out.println("MC = " + MC);
 		System.out.println("MD = " + MD);
 		*/
+
         // Compute floating-point polynomials:
-        c3 = 160.0 * Math.pow(2, -15) * AC3;
-        c4 = Math.pow(10, -3) * Math.pow(2, -15) * AC4;
-        b1 = Math.pow(160, 2) * Math.pow(2, -30) * VB1;
-        c5 = (Math.pow(2, -15) / 160) * AC5;
+        c3 = 160.0 * Math.pow(2,-15) * AC3;
+        c4 = Math.pow(10,-3) * Math.pow(2,-15) * AC4;
+        b1 = Math.pow(160,2) * Math.pow(2,-30) * VB1;
+        c5 = (Math.pow(2,-15) / 160) * AC5;
         c6 = AC6;
-        mc = (Math.pow(2, 11) / Math.pow(160, 2)) * MC;
+        mc = (Math.pow(2,11) / Math.pow(160,2)) * MC;
         md = MD / 160.0;
         x0 = AC1;
-        x1 = 160.0 * Math.pow(2, -13) * AC2;
-        x2 = Math.pow(160, 2) * Math.pow(2, -25) * VB2;
-        y0 = c4 * Math.pow(2, 15);
+        x1 = 160.0 * Math.pow(2,-13) * AC2;
+        x2 = Math.pow(160,2) * Math.pow(2,-25) * VB2;
+        y0 = c4 * Math.pow(2,15);
         y1 = c4 * c3;
         y2 = c4 * b1;
-        p0 = (3791.0 - 8.0) / 1600.0;
-        p1 = 1.0 - 7357.0 * Math.pow(2, -20);
-        p2 = 3038.0 * 100.0 * Math.pow(2, -36);
+        p0 = (3791.0-8.0) / 1600.0;
+        p1 = 1.0 - 7357.0 * Math.pow(2,-20);
+        p2 = 3038.0 * 100.0 * Math.pow(2,-36);
+
 		/*
 		System.out.println("");
 		System.out.println("c3 = " + c3);
@@ -128,28 +161,48 @@ public class BMP180 {
 		*/
     }
 
-    int startTemperature() throws ConnectionLostException, InterruptedException {
-        int delay = 5;
+    /**
+     *
+     * @return Returns the amount of milliseconds to wait.
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
+    private int startTemperature() throws ConnectionLostException, InterruptedException {
         if(writeBytes(new byte[]{(byte) BMP180_REG_CONTROL, (byte) BMP180_COMMAND_TEMPERATURE})){ //write 0x2E int reg 0xF4
-            return delay;
+            return 5;
         }
         return 0;
     }
 
+    /**
+     *
+     * @return Returns the temperature measurement from previous startTemperature method.
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
     public double getTemperature() throws ConnectionLostException, InterruptedException {
         double tu, a, t;
-        Thread.sleep(startTemperature());
+        int delay = startTemperature();
+        assert  delay > 0 : "start temperature measure fail";
+        Thread.sleep(delay);
 
         byte[] response = readBytes(new byte[]{(byte) BMP180_REG_RESULT, (byte) 0xF7}); // read reg 0xF6 (MSB), 0xF7 (LSB)
 
         tu = ((int) response[0] * 256) + (int) response[1];
-        a = c5 * (tu - c6);
-        t = a + (mc / (a + md));
+        a = c5 * (tu-c6);
+        t = a + mc/(a+md);
 
         return t;
     }
 
-    int startPressure(Oversampling oversampling) throws ConnectionLostException, InterruptedException {
+    /**
+     *
+     * @param oversampling
+     * @return Returns the amount of milliseconds to wait.
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
+    private int startPressure(Oversampling oversampling) throws ConnectionLostException, InterruptedException {
         byte[] request = new byte[2];
         request[0] = (byte) BMP180_REG_CONTROL;
         int delay;
@@ -184,39 +237,84 @@ public class BMP180 {
         return 0;
     }
 
+    /**
+     *
+     * @param temperature
+     * @param oversampling
+     * @return Returns the absolute pressure measurement from previous startPressure method.
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
     public double getPressure(double temperature, Oversampling oversampling) throws ConnectionLostException, InterruptedException {
         double pu, s, x, y, z, pressure;
-        Thread.sleep(startPressure(oversampling)); // delay
+        int delay = startPressure(oversampling);
+        assert delay > 0 : "start pressure measure fail";
+        Thread.sleep(delay); // delay
 
         byte[] response = readBytes(new byte[]{(byte) BMP180_REG_RESULT, (byte) 0xF7, (byte) 0xF8});
 
         pu = Byte.toUnsignedInt(response[0])*256.0 + Byte.toUnsignedInt(response[1]) + Byte.toUnsignedInt(response[2])/256.0;
         s = temperature - 25.0;
-        x = x2*Math.pow(s, 2) + x1*s + x0;
-        y = y2*Math.pow(s, 2) + y1*s + y0;
-        z = (pu-x) / y;
-        pressure = p2* Math.pow(z, 2) + p1*z + p0;
+        x = x2*Math.pow(s,2) + x1*s + x0;
+        y = y2*Math.pow(s,2) + y1*s + y0;
+        z = (pu-x)/y;
+        pressure = p2*Math.pow(z,2) + p1*z + p0;
 
         return pressure;
     }
 
+    /**
+     * Converts the absolute pressure to sea-level pressure (as used in weather data)
+     *
+     * @param pressure
+     * @param altitude
+     * @return Returns sea-level pressure in millibar.
+     */
     public double seaLevel(double pressure, double altitude) {
-        return (pressure / Math.pow(1 - (altitude / 44330.0), 5.255));
+        return (pressure/Math.pow(1 - altitude/44330.0,5.255));
     }
 
+    /**
+     * Converts the absolute pressure to altitude (given baseline pressure; sea-level, runway, etc.)
+     *
+     * @param pressure
+     * @param pressureAtABaseline
+     * @return Returns signed altitude in meters.
+     */
     public double altitude(double pressure, double pressureAtABaseline) {
         return (44330.0 * (1 - Math.pow(pressure / pressureAtABaseline, 1 / 5.255)));
     }
 
-    boolean readInt(byte[] request) throws ConnectionLostException, InterruptedException {
+    /**
+     *
+     * @param request The register that has to be read.
+     * @return Returns true if the read was success.
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
+    private boolean readInt(byte[] request) throws ConnectionLostException, InterruptedException {
         return twi.writeRead(BMP180_ADDRESS, SEVEN_BIT_ADDRESS, request, request.length, null, 0);
     }
 
+    /**
+     *
+     * @param request The register that has to be read.
+     * @return Returns true if the reading was successful.
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
     boolean readUInt(byte[] request) throws ConnectionLostException, InterruptedException {
         return twi.writeRead(BMP180_ADDRESS, SEVEN_BIT_ADDRESS, request, request.length, null, 0);
     }
 
-    int getIntCoefficient(byte[] request) throws ConnectionLostException, InterruptedException {
+    /**
+     *
+     * @param request The register that has to be read.
+     * @return returns the coefficient stored in the register.
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
+    private int getIntCoefficient(byte[] request) throws ConnectionLostException, InterruptedException {
         byte[] msb = new byte[1];
         byte[] lsb = new byte[1];
 
@@ -226,7 +324,14 @@ public class BMP180 {
         return 0;
     }
 
-    int getUIntCoefficient(byte[] request) throws ConnectionLostException, InterruptedException {
+    /**
+     *
+     * @param request The register that has to be read.
+     * @return returns the coefficient stored in the register.
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
+    private int getUIntCoefficient(byte[] request) throws ConnectionLostException, InterruptedException {
         byte[] msb = new byte[1];
         byte[] lsb = new byte[1];
 
@@ -236,11 +341,26 @@ public class BMP180 {
         return 0;
     }
 
-    boolean writeBytes(byte[] request) throws ConnectionLostException, InterruptedException {
+    /**
+     * Write a number of bytes to a BMP180 register.
+     *
+     * @param request
+     * @return Returns true if the writing was successful.
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
+    private boolean writeBytes(byte[] request) throws ConnectionLostException, InterruptedException {
         return twi.writeRead(BMP180_ADDRESS, SEVEN_BIT_ADDRESS, request, request.length, null, 0);
     }
 
-    byte[] readBytes(byte[] request) throws ConnectionLostException, InterruptedException {
+    /**
+     *
+     * @param request
+     * @return Returns the bytes that were requested.
+     * @throws ConnectionLostException
+     * @throws InterruptedException
+     */
+    private byte[] readBytes(byte[] request) throws ConnectionLostException, InterruptedException {
         byte[] singleResponse = new byte[1];
         byte[] totalResponse = new byte[request.length];
 
@@ -252,11 +372,21 @@ public class BMP180 {
         return totalResponse;
     }
 
+    /**
+     * Converts from millibars to atmospheres.
+     *
+     * @param pressure
+     * @return Returns the pressure's measurement in atm.
+     */
     public double mbToAtm(double pressure) {
-        return pressure * 0.000986923267;
+        return pressure*0.000986923267;
     }
 
+    /**
+     *
+     */
     public enum Oversampling {
         VERY_LOW_RES, LOW_RES, MEDIUM_RES, HIGH_RES
     }
+
 }
